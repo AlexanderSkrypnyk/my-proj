d986b82d6aa6ebb3e625967997028a5f87d6c3d7
AlexanderSkrypnyk <alexander.skrypnyk@valor-software.com>
2016-07-22 11:33:56 +0300

    feat(alert): made alert

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AlertComponent} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  directives: [AlertComponent],
+  template: `<alert type="info">ng2-bootstrap hello world!</alert>`
 })
-export class AppComponent { }
+export class AppComponent {
+}
